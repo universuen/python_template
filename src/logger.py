9b0984f11156d6d8c9e6f47030c242b23571a509
Arult <52519513+universuen@users.noreply.github.com>
2021-04-18 18:51:51 +0800

    update

--- src/logger.py
+++ src/logger.py
@@ -3,10 +3,10 @@ import settings
 
 
 class Logger(logging.Logger):
-    def __init__(self, name: str, level=settings.Log.level) -> None:
+    def __init__(self, name: str, level=settings.Logger.level) -> None:
         super().__init__(name, level=level)
-        formatter = logging.Formatter(settings.Log.format)
+        formatter = logging.Formatter(settings.Logger.format)
         handler = logging.StreamHandler()
         handler.setFormatter(formatter)
-        handler.setLevel(settings.Log.level)
+        handler.setLevel(settings.Logger.level)
         self.addHandler(handler)
--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,12 @@
-from pathlib import Path
+import pathlib
 import logging
 
 
-src_path = Path(__file__).absolute().parent
-project_path = src_path.parent
+class Path:
+    src = pathlib.Path(__file__).absolute().parent
+    project = src.parent
 
 
-class Log:
+class Logger:
     format = '[%(name)-10s] %(levelname)-8s: %(message)s'
     level = logging.DEBUG
