fa6965e6b03d638fa78ad00253f67841913aac56
Arult <52519513+universuen@users.noreply.github.com>
2021-05-31 18:03:29 +0800

    fix bugs

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ This optional file is the interface to run the whole project.
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).absolute().parent.parent))
+sys.path.insert(0, str(Path(__file__).absolute().parent.parent))
 
 if __name__ == '__main__':
     pass
