24df453ad73cfc92d7c73cc68390646fd64f2f2f
universuen <52519513+universuen@users.noreply.github.com>
2021-12-26 12:57:23 +0800

    Update path.py

--- app/config/path.py
+++ app/config/path.py
@@ -3,3 +3,7 @@ from pathlib import Path
 src: Path = Path(__file__).absolute().parent.parent
 project: Path = src.parent
 data: Path = project / 'data'
+ 
+for i in list(vars().values()):
+    if isinstance(i, Path):
+        i.mkdir(parents=True, exist_ok=True)
