db17ba3cd2fa810a83a4351ac9eee3cc9e9a7360
universuen <52519513+universuen@users.noreply.github.com>
2022-10-07 01:25:45 +0800

    update

--- src/api.py
+++ src/api.py
@@ -1,8 +1,8 @@
 import src
-from src import config
+from src import config, types
 
 
-def get_configured_logger(name: str):
+def get_configured_logger(name: str) -> types.Logger:
     return src.logger.Logger(
         name=name,
         level=src.config.Logger.level,
